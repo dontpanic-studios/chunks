819ac0a97f3ec759eeafdd3e07b784ebd8d70096
songro_ <sdbb2q@gmail.com>
2024-03-15 17:57:03 +0900

    ✅ | Feature Final(wish) testing
    - Added Json language format.
    - Bug fixed.
    - Fixed Ageable instance checker.

--- java/std/standard/chunks/events/PlayerShiftShowCropTime.java
+++ java/std/standard/chunks/events/PlayerShiftShowCropTime.java
@@ -1,8 +1,8 @@
 package std.standard.chunks.events;
 
 import com.google.common.base.Enums;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
@@ -10,6 +10,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.util.BlockIterator;
 import std.standard.chunks.enums.AvailableCrops;
+import std.standard.chunks.enums.LanguageEnum;
+import std.standard.chunks.language.Language;
 
 import java.util.List;
 
@@ -21,18 +23,26 @@ public class PlayerShiftShowCropTime implements Listener {
         Block crop = getTargetBlock(p, 1);
         TextDisplay cropStat;
 
-        if(e.isSneaking() && Enums.getIfPresent(AvailableCrops.class, crop.getType().toString()).isPresent()) {
-            Location cusLoc = new Location(p.getWorld(), crop.getX(), crop.getY() + 0.5, crop.getZ());
-            cropStat = (TextDisplay) p.getWorld().spawnEntity(cusLoc, EntityType.TEXT_DISPLAY);
-            cropStat.setBillboard(Display.Billboard.CENTER);
-            cropStat.setText("남은 시간: " + crop.getState().getData());
-        } else {
-            List<Entity> nearby =  p.getNearbyEntities(5,5,5);
-            for (Entity tmp: nearby)
-                if (tmp instanceof TextDisplay)
-                    tmp.remove();
+        try {
+            if (e.isSneaking() && Enums.getIfPresent(AvailableCrops.class, crop.getType().toString()).isPresent()) {
+                org.bukkit.block.data.Ageable ageable = ((org.bukkit.block.data.Ageable)crop.getBlockData());
+                Location cusLoc = new Location(p.getWorld(), crop.getX() + 0.5, crop.getY() + 1, p.getZ() - 0.5);
+                cropStat = (TextDisplay) p.getWorld().spawnEntity(cusLoc, EntityType.TEXT_DISPLAY);
+                cropStat.setBillboard(Display.Billboard.CENTER);
+                if (!isFullGrown(crop)) {
+                    cropStat.setText(ChatColor.COLOR_CHAR + new Language().DeSeriallizer(LanguageEnum.TEXT_CROP_AGE_LEFT) + (ageable.getMaximumAge() - ageable.getAge()));
+                } else {
+                    cropStat.setText(ChatColor.COLOR_CHAR + new Language().DeSeriallizer(LanguageEnum.TEXT_CROP_FULL_GROWN));
+                }
+            } else {
+                List<Entity> nearby = p.getNearbyEntities(5, 5, 5);
+                for (Entity tmp : nearby)
+                    if (tmp instanceof TextDisplay)
+                        tmp.remove();
+            }
+        } catch (Exception ex){
+            return;
         }
-
     }
 
     public final Block getTargetBlock(Player player, int range) {
@@ -40,11 +50,21 @@ public class PlayerShiftShowCropTime implements Listener {
         Block lastBlock = iter.next();
         while (iter.hasNext()) {
             lastBlock = iter.next();
-            if (lastBlock.getType() == Material.AIR && lastBlock.getType() != Material.WHEAT && lastBlock.getType() != Material.CARROTS && lastBlock.getType() != Material.POTATOES && lastBlock.getType() != Material.SWEET_BERRIES) {
-                continue;
+            try {
+                if (lastBlock instanceof Ageable) {
+                    continue;
+                }
+            } catch (Exception e) {
+                return null;
             }
             break;
         }
         return lastBlock;
     }
+
+    private boolean isFullGrown(Block block){
+        org.bukkit.block.data.Ageable ageable = ((org.bukkit.block.data.Ageable)block.getBlockData());
+        return ageable.getAge() >= ageable.getMaximumAge();
+    }
+
 }
